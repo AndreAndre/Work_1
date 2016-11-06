a55d9bbed99bfc88384951b6eb33fc0bf6b63b4a
Fedyushin Sergey <serfed13@gmail.com>
2016-11-06 23:44:40 +0600

    Изменения в отображении домов (HousesView). Реализована форма добавления домов и кнопка удаления домов. Использованы изображения из проекта.

--- src/main/java/views/HousesView.java
+++ src/main/java/views/HousesView.java
@@ -11,42 +11,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.sql.Date;
 import java.util.Enumeration;
 import java.util.List;
 //TODO: HTML тупо в лоб - не очень хорошо. Нужно подумать, как лучше генерировать страницы
+//TODO: комментарии и документация к методам.
 /**
  * Created by fedyu on 04.11.2016.
  */
 @WebServlet("/houses")
 public class HousesView extends HttpServlet {
 
-
     //Делаем при работе с GET-запросами
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         response.setContentType("text/html; charset=utf-8");
 
+        //Получаем список параметров и выводим на консоль (отладка)
         Enumeration<String> params =  request.getParameterNames();
-
         while (params.hasMoreElements()) {
             System.out.println("Параметр " + params.nextElement());
         }
 
 
+
+        //Параметр view передаваемый в request - отвечает за функции страницы
         String paramView = request.getParameter("view");
         if (paramView == null || paramView.isEmpty())
             printHousesTable(request, response);
         else {
             switch (paramView) {
+                //add - добавляем новую запись в таблицу домов
                 case "add":
-                    printAddNewHouseForm(request, response);
+                    String paramAddress = request.getParameter("address");
+                    int paramFloors =  Integer.parseInt(request.getParameter("floors"));
+                    Date paramBuildDate = Date.valueOf(request.getParameter("buildDate"));
+                    addNewHouse(paramAddress, paramFloors, paramBuildDate);
+                    printHousesTable(request, response);
+                    printSuccessfulAddMessage(response);
                     break;
+                //Выводим список всех домов
                 case "list":
                     printHousesTable(request, response);
                     break;
+                //Удаляем дом из таблицы домов
                 case "del":
-                    int paramDelete = Integer.getInteger(request.getParameter("removeId"));
+                    int paramDelete = Integer.parseInt(request.getParameter("removeId"));
                     System.out.println("paramDelete = " + paramDelete);
                     removeHouse(paramDelete);
                     printHousesTable(request, response);
@@ -56,17 +67,43 @@ public class HousesView extends HttpServlet {
                     break;
             }
         }
-
-
     }
 
+
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException
-    {
+            throws IOException, ServletException    {
         doGet(request, response);
     }
 
+    /**
+     * Метод выводит сообщени об успешной операции
+     * @param response
+     * @throws IOException
+     */
+    private void printSuccessfulAddMessage(HttpServletResponse response) throws IOException {
+        response.getWriter().print("</br><img src='./images/success_128.png'><h2>Дом успешно добавлен</h2>");
+    }
 
+    /**
+     * Метод добавляет новый дом в БД
+     * @param paramAddress - Адрес дома
+     * @param paramFloors - количество этажей
+     * @param paramBuildDate - дата постройки
+     */
+    private void addNewHouse(String paramAddress, int paramFloors, Date paramBuildDate) {
+        EntityUtilsImpl entityUtils = new EntityUtilsImpl();
+        HousesEntity newHouse = new HousesEntity(paramAddress, paramFloors, paramBuildDate);
+        entityUtils.add(newHouse);
+    }
+
+
+
+    /**
+     * Метод отображает таблицу с домами
+     * @param request
+     * @param response
+     * @throws IOException
+     */
     private void printHousesTable(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         PrintWriter htmlPage = response.getWriter();
@@ -81,9 +118,7 @@ public class HousesView extends HttpServlet {
             "<td>Адрес дома</td>",
             "<td>Количество этажей</td>",
             "<td>Дата постройки</td>",
-            "<td width=\"40\"></td>",
-            "<td width=\"40\"></td>",
-            "<td width=\"40\"></td>",
+            "<td width=\"120\"></td>",
            "</tr>");
 
         for (HousesEntity house :
@@ -92,37 +127,40 @@ public class HousesView extends HttpServlet {
                 "<td>"+house.getAddress()+"</td>",
                 "<td>"+house.getFloors()+"</td>",
                 "<td>"+house.getBuildDate()+"</td>",
-                "<td><a href='./apartments?houseId=" + house.getId() + "'><img src='https://cdn2.iconfinder.com/data/icons/bitsies/128/City-32.png'></a></td>",
-                "<td><a href='#'><img src='https://cdn2.iconfinder.com/data/icons/bitsies/128/EditDocument-32.png'></a></td>",
-                "<td><a href='?view=del&removeId=" + house.getId() + "'><img src='https://cdn2.iconfinder.com/data/icons/bitsies/128/Cancel-32.png'></a></td>",
+
+                "<td><a href='./apartments?houseId=" + house.getId() + "'><img src='./images/apartments_32.png'></a>",
+                "<a href='#'><img src='./images/edit_32.png'></a>",
+                "<a href='?view=del&removeId=" + house.getId() + "'><img src='./images/remove_32.png'></a></td>",
                 "</tr>");
         }
 
+
+
+        TextUtils.println(response,
+                "<form action='./houses' method=GET>",
+                "<tr height='40'>",
+                "<td><input type='text' placeholder='Адрес нового дома' name='address' style='width:100%; height:40px; border:0'></td>",
+                "<td><input type='number' placeholder='Этажей' min=1 name='floors' style='width:100%; height:40px; border:0'></td>",
+                "<td><input type='date' placeholder='Дата постройки' name='buildDate' style='width:100%; height:40px; border:0'></td>",
+                "<input type='hidden' name='view' value='add'>",
+                "<td><input type='submit' value='Добавить дом'></td>",
+                "</tr>",
+                "</form>"
+        );
+
         htmlPage.println("</table>");
-        htmlPage.println("<a href='?view=add'><img src='https://cdn2.iconfinder.com/data/icons/bitsies/128/Add-32.png'>Добавить новую запись.</a> ");
         //
 
         //Закрываем коннект к БД
         //HibernateSessionFactory.shutdown();
     }
 
-    //TODO: Реализовать форму по добавлению нового дома в БД
-    private void printAddNewHouseForm(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        EntityUtilsImpl entityUtils = new EntityUtilsImpl();
 
-        PrintWriter htmlPage = response.getWriter();
-        htmlPage.print("<h1>Дома</h1>");
-        htmlPage.print("Добавляем новый дом");
-
-        String formAddress = "ул. Балбеса, 22";
-        int formFloors = 5;
-        Date formDate = Date.valueOf("1980-01-02");
-
-        HousesEntity newHouse = new HousesEntity(formAddress, formFloors, formDate);
-        entityUtils.add(newHouse);
-    }
-
-    //TODO: Удаление домов (сейчас NullPointerException)
+    /**
+     * Мутод получает ID-дома и удаляет его из БД
+     * @param houseId
+     * @throws IOException
+     */
     private void removeHouse(int houseId) throws IOException {
         System.out.println("remove houseId = " + houseId);
         EntityUtilsImpl entityUtils = new EntityUtilsImpl();
